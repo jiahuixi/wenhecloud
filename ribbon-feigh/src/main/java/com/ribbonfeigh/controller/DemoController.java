8182dec876ffb0a050700058cf3475c7d9993f64
jiahuixi <791226357@qq.com>
2019-06-05 11:19:48 +0800

    dasda

---
+++ ribbon-feigh/src/main/java/com/ribbonfeigh/controller/DemoController.java
@@ -0,0 +1,8 @@
+package com.ribbonfeigh.controller;
+
+/**
+ * @author jiahuixi
+ * @date 2019/6/5 11:16
+ */
+public class DemoController {
+}
---
+++ ribbon-feigh/src/test/java/com/ribbonfeigh/RibbonFeighApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ribbonfeigh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RibbonFeighApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
